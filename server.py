c39a86cbd89bc20e2bbfccdfa542da1b909281ae
Faris Ansara <ansara.fares@gmail.com>
2023-12-01 00:03:35 +0000

    finished the get_skills function, need to check with prof smthn. Started the create class function, do todo's

--- server.py
+++ server.py
@@ -24,6 +24,8 @@ import random  # generate random numbers
 import time  # needed to record when stuff happened
 import datetime
 
+import numpy
+import numpy as np
 
 def random_digits(n):
     """This function provides a random integer with the specfied number of digits and no leading zeros."""
@@ -111,6 +113,16 @@ def build_response_redirect(where):
        If this action is used, it should be the only response provided."""
     return {"type": "redirect", "where": where}
 
+def check_if_session_invalid(imagic, iuser):
+    return do_database_fetchone(f'SELECT magic FROM session WHERE userid = {iuser}')[0] == imagic
+
+def check_username_in_database(username):
+    res = do_database_fetchone(f'SELECT * FROM users WHERE username = "{username}"')
+    return bool(res)
+
+def check_password_for_username(username, password):
+    res = do_database_fetchone(f'SELECT password FROM users WHERE username = "{username}"')[0]
+    return res == password
 
 # The following handle_..._request functions are invoked by the corresponding /action?command=.. request
 def handle_login_request(iuser, imagic, content):
@@ -135,18 +147,11 @@ def handle_login_request(iuser, imagic, content):
         return [iuser, imagic, response]
     imagic = random_digits(10)
     iuser = do_database_fetchone(f'SELECT userid FROM users WHERE username = "{username}"')[0]
+    # Todo: it is not logging out previous sessions even though the session table updates here, it doesnt seem to update all together.
     do_database_execute(f'DELETE FROM session WHERE userid = "{iuser}"')
     do_database_execute(f'INSERT INTO session (userid, magic) VALUES ({iuser},{imagic})')
     response.append({"type": "redirect", "where": "\index.html"})
     return [iuser, imagic, response]
-
-def check_username_in_database(username):
-    res = do_database_fetchone(f'SELECT * FROM users WHERE username = "{username}"')
-    return bool(res)
-
-def check_password_for_username(username, password):
-    res = do_database_fetchone(f'SELECT password FROM users WHERE username = "{username}"')[0]
-    return res == password
 
 def handle_logout_request(iuser, imagic, parameters):
     """This code handles the selection of the logout button.
@@ -154,27 +159,143 @@ def handle_logout_request(iuser, imagic, parameters):
     need to ensure the end of the session is recorded in the database
         And that the session magic is revoked."""
     response = []
-    ## Add code here
-    is_in_session = do_database_execute(f'SELECT * FROM session WHERE magic= {imagic} AND userid = {iuser}')
-    if is_in_session:
-        do_database_execute(f'DELETE FROM session WHERE magic = {imagic}')
-        response.append({"type": "redirect", "where": "\logout.html"})
-    else:
-        response.append({"type": "redirect", "where": "\login.html"})
+    check_session = check_if_session_invalid(imagic, iuser)
+    if not check_session:
+        response.append({"type": "redirect", "where": "/login.html"})
+        return [iuser, imagic, response]
+
+    response.append({"type": "redirect", "where": "\logout.html"})
     return [iuser, imagic, response]
 
+def format_my_returns(tuple_in):
+    out = []
+    for val in tuple_in:
+        out.append(val[0])
+    return tuple(out)
+
+def get_skills_where_user_is_trainer_and_clean_array(my_skills, user_is_trainer):
+    for arr in user_is_trainer:
+        trainer_id = arr[0]
+        skill_id = arr[1]
+        skill_name = do_database_fetchone(f'SELECT name FROM skill WHERE skillid = {skill_id}')[0]
+        trainer_name = do_database_fetchone(f'SELECT fullname FROM users WHERE userid = {trainer_id}')[0]
+        gained = None
+        status = "trainer"
+        my_skills = numpy.append(my_skills, [[skill_id, skill_name, trainer_name, gained, status]], axis=0)
+    indices_to_delete = []
+    for i, row in enumerate(my_skills):
+        if row[4] in ["cancelled", "removed"]:
+            indices_to_delete.append(i)
+        elif row[4] == 'enrolled':
+            if int(row[3]) < int(time.time()):
+                my_skills[i][4] = "scheduled"
+            elif int(row[3]) >= int(time.time()):
+                my_skills[i][4] = "pending"
+    my_skills = np.delete(my_skills, indices_to_delete, axis=0)
+    return my_skills
+
+def get_states_of_users(statuses):
+    states = []
+    for status in statuses:
+        match (status):
+            case 0:
+                states.append("enrolled")
+            case 1:
+                states.append("passed")
+            case 2:
+                states.append("failed")
+            case 3:
+                states.append("cancelled")
+            case 4:
+                states.append("removed")
+            case other:
+                states.append(None)
+    return states
+
+def get_trainer_names(trainer_ids):
+    if isinstance(trainer_ids, int):
+        return do_database_fetchone(f'SELECT fullname FROM users WHERE userid = {trainer_ids}')[0]
+    trainer_names = []
+    for id in trainer_ids:
+        name = do_database_fetchone(f'SELECT fullname FROM users WHERE userid = {id}')
+        if name:
+            trainer_names.append(name[0])
+    return trainer_names
+
+def get_skill_names(skill_ids):
+    if isinstance(skill_ids, int):
+        return do_database_fetchone(f'SELECT name FROM skill WHERE skillid = {skill_ids}')[0]
+    skills = []
+    for val in skill_ids:
+        skill = do_database_fetchone(f'SELECT name FROM skill WHERE skillid = {val}')
+        if skill:
+            skills.append(skill[0])
+    return skills
+
+def get_skillids_trainerids_start(class_ids):
+    skill_ids = []
+    trainer_ids = []
+    start_dates = []
+    for id in class_ids:
+        out = do_database_fetchone(f'SELECT skillid, trainerid, start FROM class WHERE classid = {id}')
+        if out:
+            skill_ids.append(out[0])
+            trainer_ids.append(out[1])
+            start_dates.append(out[2])
+    return skill_ids, start_dates, trainer_ids
+
+def get_my_skills_array(class_ids, statuses):
+    my_skills = []
+
+    skill_ids, start_dates, trainer_ids = get_skillids_trainerids_start(class_ids)
+    my_skills.append(skill_ids)
+
+    skills = get_skill_names(skill_ids)
+    my_skills.append(skills)
+
+    trainer_names = get_trainer_names(trainer_ids)
+    my_skills.append(trainer_names)
+
+    my_skills.append(start_dates)
+
+    states = get_states_of_users(statuses)
+    my_skills.append(states)
+
+    my_skills = np.array(my_skills).transpose()
+    user_is_trainer = do_database_fetchall(f'SELECT trainerid, skillid FROM trainer WHERE trainerid = 1')
+
+    my_skills = get_skills_where_user_is_trainer_and_clean_array(my_skills, user_is_trainer)
+
+    return my_skills
 
 def handle_get_my_skills_request(iuser, imagic):
     """This code handles a request for a list of a users skills.
        You must return a value for all vehicle types, even when it's zero."""
 
     response = []
-    #
+    print(iuser, imagic)
+    check_session = check_if_session_invalid(imagic, iuser)
+    print(check_session)
+    if not check_session:
+        response.append({"type": "redirect", "where": "/login.html"})
+        return [iuser, imagic, response]
 
-    ## Add code here
+    class_ids = format_my_returns(do_database_fetchall(f'SELECT classid FROM attendee WHERE userid = {iuser}'))
+    statuses = format_my_returns(do_database_fetchall(f'SELECT status FROM attendee WHERE userid = {iuser}'))
+    my_skills = get_my_skills_array(class_ids, statuses)
+    for row in my_skills:
+        dic = {
+            "type": "skill",
+            "id": row[0],
+            "name": row[1],
+            "trainer": row[2],
+            "gained": row[3],
+            "state": row[4]
+        }
+        response.append(dic)
 
+    # Todo: CHECK FORUM, is the trainer in the class?
     return [iuser, imagic, response]
-
 
 def handle_get_upcoming_request(iuser, imagic):
     """This code handles a request for the details of a class.
@@ -185,7 +306,6 @@ def handle_get_upcoming_request(iuser, imagic):
     ## Add code here
 
     return [iuser, imagic, response]
-
 
 def handle_get_class_detail_request(iuser, imagic, content):
     """This code handles a request for a list of upcoming classes.
@@ -232,7 +352,6 @@ def handle_update_attendee_request(iuser, imagic, content):
     """This code handles a request to cancel a user attendance at a class by a trainer"""
 
     response = []
-
     ## Add code here
 
     return [iuser, imagic, response]
@@ -242,9 +361,33 @@ def handle_create_class_request(iuser, imagic, content):
     """This code handles a request to create a class."""
 
     response = []
+    skill_id = content['id']
+    day = content['day']
+    month = content['month']
+    year = content['year']
+    hour = content['hour']
+    minute = content['minute']
+    note = content['note']
+    max_students = content['max']
 
-    ## Add code here
+    skill_name = get_skill_names(skill_id)
+    trainer_name = get_trainer_names(iuser)
 
+    try:
+        date_time = int(datetime.datetime(year, month, day, hour, minute).timestamp())
+        print("Datetime created:", date_time)
+    except ValueError as e:
+        response.append(build_response_message(204, 'Invalid Date or Time.'))
+        return [iuser, imagic, response]
+
+    if max_students > 10 or max_students < 1:
+        response.append(build_response_message(205, 'Max class size should be between 1-10 students.'))
+
+    # Todo: Get actual size of class which is the number of student currently on the class
+    size = 1
+    # Todo: add new class into class table, if trainer in the attendee table, add there as well.
+    # Todo: check response, the action bit?
+    response.append({"type": "class", "id": skill_id, "name": skill_name, "trainer": trainer_name, "notes": note, "when": date_time, "size": size, "max": max_students, "action": "edit"})
     return [iuser, imagic, response]
 
 
@@ -462,18 +605,23 @@ def run():
     print('running server on port =', sys.argv[1], '...')
     httpd.serve_forever()  # This function will not return till the server is aborted
 
+
 run()
 
 
-## SQL QUERIES:
 
-('INSERT into attendee (userid, classid, status) VALUES (1,1, "pending"), '
- '(1,2,"passed"),'
- ' (1,3,"failed"), '
- '(2,2,"pendiing"), '
- '(2,1,"failed"), '
- '(2,3,"passed")')
+## SQL QUERIES
 
-'INSERT into class (trainerid, skillid, start, "max", note) VALUES (1,1,1234567,10,"KENNN")'
+('INSERT into attendee (userid, classid, status) VALUES (1,1,0), '
+ '(1,2,1),'
+ ' (1,3,2), '
+ '(2,2,3), '
+ '(2,1,4), '
+ '(2,3,1)')
 
-# 'INSERT INTO session (userid, magic) VALUES ("3ans02", 6546138)'
+'INSERT into class (trainerid, skillid, start, "max", note) VALUES (1,1,1234567,10,"STFDS"), (1,1,1234578,20,"Applied DataScience"), (2,2,1234589,25, "Stats")'
+
+'INSERT into skill (name) VALUES ("Python"), ("Statistics and Probability")'
+
+'INSERT into trainer values (1,1),(2,2)'
+
